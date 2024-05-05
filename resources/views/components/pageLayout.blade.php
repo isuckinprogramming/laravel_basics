197b0dd5cd5f3d6fa16c71c8b70b77e06a267a98
Borgs <nisiyawa55@gmail.com>
2024-05-05 14:36:11 -0700

    Create General use case components
    Create components that can be used for general use case.
    
    The navigation bar will probably be reused by more
    than 3 pages. I think it will create problems with
    implementing the shadow to indicate current page inside
    the nav bar.
    
    I want to reuse the standard head and body layout
    for most pages. So that the imports for the links
    and scripts can be placed in one file only.

---
+++ resources/views/components/pageLayout.blade.php
@@ -0,0 +1,17 @@
+<div>
+    <!-- He who is contented is rich. - Laozi -->
+</div>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> @yield("title","page layout default title") </title>
+    @yield("head-content")
+</head>
+<body>
+    @yield("body-content","default body content")
+</body>
+</html>
--- resources/views/components/pageNav.blade.php
+++ resources/views/components/pageNav.blade.php
@@ -1,9 +1,9 @@
 <div>
     <!-- An unexamined life is not worth living. - Socrates -->
     <nav>
-        <a href="#">Home</a>
-        <a href="#">Orders</a>
-        <a href="#">Account</a>
-        <a href="#">Settings</a>
+        <a href="{{route("homepage")}}">Home</a>
+        <a href="{{route("orders")}}">Orders</a>
+        <a href="{{route("settings")}}">Settings</a>
+        <a href="{{route("accounts")}}">Account</a>
     </nav>
 </div>
