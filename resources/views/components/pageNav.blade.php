582deb5794edfa5272839b4c32e27c5f79177c91
Borgs <nisiyawa55@gmail.com>
2024-05-05 13:34:00 -0700

    Base blade files for Pages
    Created the required pages. Create blade pages to show
    the users.
    
    No content yet. The contents to be added for the
    settings and accounts pages is yet to be decided.
    
    I don't know what to put inside.

---
+++ resources/views/components/pageNav.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <!-- An unexamined life is not worth living. - Socrates -->
+    <nav>
+        <a href="#">Home</a>
+        <a href="#">Orders</a>
+        <a href="#">Account</a>
+        <a href="#">Settings</a>
+    </nav>
+</div>
---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,19 @@
+
+<!--
+I have not failed.
+I've just found 10,000 ways that won't work. - Thomas Edison -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Homepage</title>
+
+    <link rel="stylesheet" href="./../../">
+
+</head>
+<body>
+    @extends("components/pageNav")
+</body>
+</html>
