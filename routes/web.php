5b74c12175161074f936bc5f74686a42407a9fc9
Borgs <nisiyawa55@gmail.com>
2024-05-05 14:45:46 -0700

    Update Named Routes to access all pages

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,40 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('homepage');
+})->name("homepage");
+
+Route::get(
+    '/register account',
+    function () {
+        return view('/registerAccount');
+    }
+)->name("registerAccount");
+
+Route::get(
+    '/login account',
+    function () {
+        return view('/loginAccount');
+    }
+)->name("loginAccount");
+
+Route::get(
+    '/orders',
+    function () {
+        return view('/orders');
+    }
+)->name("orders");
+
+Route::get(
+    '/accounts',
+    function () {
+        return view('/accounts');
+    }
+)->name("accounts");
+
+Route::get(
+    '/settings',
+    function () {
+        return view('/settings');
+    }
+)->name("settings");
