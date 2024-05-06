d7791dfae02fe70e4e10818b1caf7103a4499d4b
Borgs <nisiyawa55@gmail.com>
2024-05-06 04:56:42 -0700

    Fix: Update & Delete no longer use Ajax
    Using ajax when the page needs to reload is a mistake
    in my part.
    
    I want the page to reload after the request, but
    since ajax was used to create the request
    the page was not reloading.
    
    Visited stackoverflow for advice on the problem.
    
    Got Recommended to not use ajax if page will reload
    and the fix I chose is to change the window location
    with a url combined with the GET data.
    
    Not secure but works fast for development.

--- public/js/ordersDeleteAndUpdate.js
+++ public/js/ordersDeleteAndUpdate.js
@@ -9,19 +9,22 @@ function passDataForCRUDRequest (){
     const entryId =  $(this).data("entry-id")
     console.log("entry id : " + entryId);
 
-    $.ajax({
-        method: "POST",
-        url:"update-order-start",
-        data: { id: entryId },
-        dataType: "JSON",
-        success: function (response) {
+    window.location = "update-entry-start?id=" + entryId;
+    // $.ajax({
+    //     method: "GET",
+    //     url:"update-entry-start",
+    //     data: { id: entryId },
+    //     dataType: "JSON",
+    //     success: function (response) {
 
-            console.log( response + "\nentry update start: " + entryId)
-        },
-        error: function (e) {
-            console.log(e);
-        }
-    });
+    //         console.log(response + "\nentry update start: " + entryId)
+
+    //         // document.getElementsByTagName("body").innerHtml = response.reponseText;
+    //     },
+    //     error: function (e) {
+    //         console.log(e);
+    //     }
+    // });
 }
 
 $(".update-trigger").on(
@@ -29,6 +32,13 @@ $(".update-trigger").on(
     passDataForCRUDRequest
 )
 
+$(".delete-trigger").on(
+    "click",
+    function(){
+        const entryId =  $(this).data("entry-id")
+        window.location = "delete-order-entry?id=" + entryId;
+    }
+)
 // $(".delete-trigger").on(
 //     "click",
 //     // passDataForCRUDRequest
